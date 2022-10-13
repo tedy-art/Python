8e640129a5ad42542c9a95426a60c1abf1637bbd
Tejas Falke <tejasdf.77@gmail.com>
2022-10-13 09:37:03 +0530

    add comment

--- 03.Data_Type/04.List/12.insert_less_values_in_list.py
+++ 03.Data_Type/04.List/12.insert_less_values_in_list.py
@@ -1,5 +1,7 @@
 thislist = ["apple","banana","cherry"]
 thislist[1:3] = ["watermelon"]
 print(thislist)
-#if you insert less items than you replace, the new items will be inserted where you
-# opecified, and the remaining items will move accordingly.
+"""
+if you insert less items than you replace, the new items will be inserted where you
+opecified, and the remaining items will move accordingly.
+"""
