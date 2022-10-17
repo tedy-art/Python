b7b96978c17a00cd062002d082424b02200f8461
Tejas Falke <tejasdf.77@gmail.com>
2022-10-17 20:18:27 +0530

    linked List

---
+++ 02.Data_structures_&_Algorithms/01.Array & Linked List/02.Linked_list/01.Implementation_of_linkedList.py
@@ -0,0 +1,29 @@
+# Linked list Implementation
+class Node:
+    # creating a node
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+if __name__ == '__main__':
+    linked_list = LinkedList()
+
+    # Assign item values
+    linked_list.head = Node(1)
+    Second = Node(2)
+    Third = Node(3)
+
+    # Connect nodes
+    linked_list.head.next = Second
+    Second.next = Third
+
+    # Print the linked list item
+    while linked_list.head != None:
+        print(linked_list.head.item,end=" ")
+        linked_list.head = linked_list.head.next
