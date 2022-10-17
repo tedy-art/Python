4b2d642f2fdcf8fa56945e3c263426caf7627d67
Tejas Falke <tejasdf.77@gmail.com>
2022-10-17 10:15:58 +0530

    various way to create an array

--- 02.Data_structures_&_Algorithms/01.Array & Linked List/01.array/01.Array.py
+++ 02.Data_structures_&_Algorithms/01.Array & Linked List/01.array/01.Array.py
@@ -1,5 +1,8 @@
 # Import array
+# from array import *
 import array as arr
 
+# number = array.array('i',10,20,30)
+# number = array('i',[10,20,30])
 numbers= arr.array('i',[10,20,30])
 print(numbers)
