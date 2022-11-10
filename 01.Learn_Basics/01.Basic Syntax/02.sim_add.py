54c93e7e21809d85e35839907ffc641736928d71
Tejas Falke <tejasdf.77@gmail.com>
2022-11-11 01:29:07 +0530

    Blackbox code testing

---
+++ 01.Learn_Basics/01.Basic Syntax/02.sim_add.py
@@ -0,0 +1,10 @@
+# write a python code for add two digits?
+def solveMeFirst(a,b):
+
+   return a+b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+res = solveMeFirst(num1,num2)
+print(res)
+
