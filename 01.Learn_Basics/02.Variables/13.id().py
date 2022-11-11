7ee05ce7a3d08922de2c894222935ac493cf208f
Tejas Falke <tejasdf.77@gmail.com>
2022-11-11 23:07:43 +0530

    id()

--- 01.Learn_Basics/02.Variables/13.id().py
+++ 01.Learn_Basics/02.Variables/13.id().py
@@ -3,6 +3,6 @@ b = 20
 c = a + b
 print("Add : ", c)
 
-print("Address of a : ", id(a))
-print("Address of b : ", id(b))
-print("Address of c : ", id(c))
+print("Address of a(Memory Loc) : ", id(a))
+print("Address of b(Memory Loc) : ", id(b))
+print("Address of c(Memory Loc) : ", id(c))
