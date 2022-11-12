66b2ecd63f7fa22b230390d581441c24988332c0
Tejas Falke <tejasdf.77@gmail.com>
2022-11-12 22:47:23 +0530

    Balanced binary tree

---
+++ 02.Data_structures_&_Algorithms/03.Tree/01.Types/04.Balanced_Binary_Tree.py
@@ -0,0 +1,31 @@
+# Cheking if a binary is height balaned in python
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+
+class Height:
+    def __init__(self):
+        self.height = 0
+
+def isHeightBalanced(root, height):
+    left_height = Height()
+    right_height = Height()
+    if root is None:
+        return True
+
+    l = isHeightBalanced(root.left, left_height)
+    r = isHeightBalanced(root.right, right_height)
+
+    height.height = max(left_height.height.right_height.height) + 1
+
+    if abs(left_height.height - right_height.height) <= 1:
+        return l and r
+    return False
+
+height = Height()
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
