15fc23e18a13b3097e78951c0bf302afbe851d60
Tejas Falke <tejasdf.77@gmail.com>
2022-11-21 11:56:47 +0530

    string fata types

---
+++ 100.The_K_Academy/Bac/Conditionals/TKA/Assignments TKAcadamy/as1.py
@@ -0,0 +1,17 @@
+# Converting decimal to binary
+a = 23
+print("Decimal : ",a)
+x = bin(a)
+print("convert decimal to binary : ",x)
+
+# Converting decimal to octal
+b = 34222
+print('Decimal : ',b)
+x = oct(b)
+print("convert decimal to octal : ",x)
+
+# Converting decimal to hexa
+c = 2342
+print("Decimal : ",c)
+x = hex(c)
+print("Convert decimal to hexa : ", c)
