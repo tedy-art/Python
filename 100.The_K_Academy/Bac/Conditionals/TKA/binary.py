773ec839c8b9ca5d637c8cd96014203259961029
Tejas Falke <tejasdf.77@gmail.com>
2022-11-21 10:45:32 +0530

    Complex data type

--- 100.The_K_Academy/Bac/Conditionals/TKA/binary.py
+++ 100.The_K_Academy/Bac/Conditionals/TKA/binary.py
@@ -1,14 +1,14 @@
-# binary
+# binary [range 0 to 1, base 2]
 my_var = 0b10101
 print(type(my_var))
 print(my_var)
 
-# Octal
+# Octal [range 0 to 7, base 8]
 my_var = 0o342
 print(type(my_var))
 print(my_var)
 
-# Hexadecimal num
+# Hexadecimal [range 0 to 9 and A to F, base 16]
 my_var = 0xFace23
 print(type(my_var))
 print(my_var)
