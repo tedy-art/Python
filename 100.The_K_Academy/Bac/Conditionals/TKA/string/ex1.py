86ad1bcb64281f49e35c9ba3687b43104735f941
Tejas Falke <tejasdf.77@gmail.com>
2022-11-21 12:00:32 +0530

    new

---
+++ 100.The_K_Academy/Bac/Conditionals/TKA/string/ex1.py
@@ -0,0 +1,5 @@
+# WAP to print element from given string from its index value.Ask user to enter index value
+
+a = "Instagram"
+b = int(input("index"))
+print(a[b])
---
+++ 100.The_K_Academy/Bac/Conditionals/TKA/string/ex2.py
@@ -0,0 +1,5 @@
+s = "Instagram"
+print(s[1 : 6])
+print(s[-1 : -5])
+print(s[1 : 6])
+print(s[6 : -7])
