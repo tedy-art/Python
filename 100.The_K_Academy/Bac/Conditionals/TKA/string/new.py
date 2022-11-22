ea6066843586a2662d5af629180f28db6024d90a
Tejas Falke <tejasdf.77@gmail.com>
2022-11-22 12:42:03 +0530

    slice -ve and +ve

---
+++ 100.The_K_Academy/Bac/Conditionals/TKA/string/new.py
@@ -0,0 +1,10 @@
+name = "anupam"
+# print(name.index("a")) # index() is show index number
+# print(name[-1:-3:2])
+# print(name[-1:-6:2])
+a = name[-2:-9:-1]
+print(a)
+
+# name = "rajesh"
+# x = name[2::-2]
+# print(x)
