1f8188c7e0497b4507389dfa577e2ed599857a07
Tejas Falke <tejasdf.77@gmail.com>
2022-11-24 22:48:30 +0530

    string 23/11/2022

--- 100.The_K_Academy/Bac/Conditionals/TKA/string/try.py
+++ 100.The_K_Academy/Bac/Conditionals/TKA/string/try.py
@@ -1,2 +1,15 @@
-s = "Tejas"
-print(s[::3])
+# user_name = input("Enter your name : ")
+# print(type(user_name))
+# com_name = input("Enter your company name : ")
+# print(user_name+"."+com_name+"@mail.com")
+# email = user_name + com_name
+# print(email+"@gmail.com")
+
+# num = int(input("Enter num:"))
+# print(type(num))
+# user_int = int(input("Enter int : "))
+# user_float = float(input("Enter float : "))
+# sum_total = user_int + user_float
+# print("O/p : ",sum_total)
+# print(type(sum_total))
+
