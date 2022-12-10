ba3cedf4cabb93fc373f8da88c399891a217625b
Tejas Falke <tejasdf.77@gmail.com>
2022-12-10 18:05:52 +0530

    greater and less number in list

---
+++ 100.The_K_Academy/TKA/Operators/03_Relational_operator/Ass3.py
@@ -0,0 +1,9 @@
+l = [123, 45, -56, 98, 99, 100, 456, 89, 458]
+for i in l:
+    if i >= 100 and i < 300:
+        print(i)
+print('***'* 20)
+
+for j in l:
+    if i >= 100 or i < 300:
+        print(i)
