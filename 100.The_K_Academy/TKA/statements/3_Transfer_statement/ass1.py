34bf04bec789d8769e9bff58c8e8d5ed1e7a039b
Tejas Falke <tejasdf.77@gmail.com>
2022-12-10 18:02:11 +0530

    break assignment

---
+++ 100.The_K_Academy/TKA/statements/3_Transfer_statement/ass1.py
@@ -0,0 +1,20 @@
+"""--------------------08/12/2022-------------------------------------"""
+"""--------------------Break-------------------------------------"""
+
+
+# for i in "INSTAGRAM":
+#     print(i)
+#     if i == 'G':
+#         break
+
+# while True:
+#     print("Python is fun...")
+#     ch = input("Do you want to cuntiue [y/n] : ")
+#     if ch == 'n':
+#         break
+
+while True:
+    print("Python is fun...")
+    ch = input("Do you want to cuntiue [y/n] : ")
+    if ch.lower() != 'y':
+        break
