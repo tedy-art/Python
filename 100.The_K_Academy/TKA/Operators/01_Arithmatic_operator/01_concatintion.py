c0936f642f0e470e9004964dc146a9928a06ff97
Tejas Falke <tejasdf.77@gmail.com>
2022-12-10 17:51:10 +0530

    Concatintion

---
+++ 100.The_K_Academy/TKA/Operators/01_Arithmatic_operator/01_concatintion.py
@@ -0,0 +1,14 @@
+s = "INSTAGRAM"
+s1 = "FACEBOOK"
+s2 = s + s1
+print(s2)
+
+s = ["INSTAGRAM"]
+s1 = ["FACEBOOK"]
+s2 = s + s1
+print(s2)
+
+s = ("INSTAGRAM")
+s1 = ("FACEBOOK")
+s2 = s + s1
+print(s2)
