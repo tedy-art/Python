b2e561524246257406d42245964714243ff9f555
Tejas Falke <tejasdf.77@gmail.com>
2022-12-12 23:32:10 +0530

    Start To inmprove my code

---
+++ 100.The_K_Academy/Functional_Programming/01_print_with_func.py
@@ -0,0 +1,9 @@
+print("First line of the function.")
+# Creating a function using def keyword
+def print_func():
+    i = 'Tejas'
+    print("Inside the function.")
+    print(i)
+
+print("Ourside of the function.")
+print_func()
