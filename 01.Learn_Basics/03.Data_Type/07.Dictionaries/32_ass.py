a4918ed4162eb9831abe55a43b77f1743953532d
Tejas Falke <tejasdf.77@gmail.com>
2022-12-16 11:41:09 +0530

    Access dict

--- 01.Learn_Basics/03.Data_Type/07.Dictionaries/32_ass.py
+++ 01.Learn_Basics/03.Data_Type/07.Dictionaries/32_ass.py
@@ -1,1 +1,18 @@
-d = 
+# Creating a dictionary
+Dict = {}
+print("Empty Dictionary : ")
+print(Dict)
+
+# creating dictionary using dict() method
+D = dict({1:"hi", 2:"hello", 3:"Welcome"})
+print("\ndict() method : ")
+print(D)
+
+Dict = dict({1: 'Geeks', 2: 'For', 3: 'Geeks'})
+print("\nDictionary with the use of dict(): ")
+print(Dict)
+
+# Create a dictionary with each item as Pair
+Dict = dict([(1 , 'hi'), (2 , 'Hello')])
+print("\nDictionary with each item as pair : ")
+print(Dict)
