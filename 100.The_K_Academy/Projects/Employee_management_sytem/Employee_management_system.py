63f066fdeb97bf0fe957d24590f591c3b1596886
Tejas Falke <tejasdf.77@gmail.com>
2022-12-19 23:51:22 +0530

    emp mang sys

--- 100.The_K_Academy/Projects/Employee_management_sytem/Employee_management_system.py
+++ 100.The_K_Academy/Projects/Employee_management_sytem/Employee_management_system.py
@@ -3,11 +3,11 @@ db = {101:{'name':'Jay', 'address': 'Talegoan', 'id': 101, 'salary':20000}}
 def dashboard():
     print("\t\t Welcome to Employee Management System")
     print("""
-            Manu
-            1) Create new Employee record
-            2) Read Employee record
-            3) update employee reocrd
-            4) Delete employee record
+                Manu
+                1) Create new Employee record
+                2) Read Employee record
+                3) update employee reocrd
+                4) Delete employee record
     """)
 
 def create_emp():
@@ -38,10 +38,28 @@ def read_emp():
         print('-'*65)
 
 def update_emp():
-    pass
+    u_id = eval(input("Enter employee id for Update : "))
+    if u_id in db:
+        u_name = input("Enter employee name for update : ")
+        u_address = input("Enter emplyee address for update : ")
+        u_salary = eval(input("Enter employee salary for update : "))
+
+        db[u_id]['name'] = u_name
+        db[u_id]['address'] = u_address
+        db[u_id]['salary'] = u_salary
+        print(f"Employee {db[u_id]['name']} updated successfully in db...")
+        print('*'*65)
+        print()
 
 def delete_emp():
-    pass
+    u_id = eval(input("Enter student roll to delete : "))
+
+    if u_id in db:
+        n = db[u_id]['name']
+        del db[u_id]
+        print(f"Employee {n} deleted successfully from database...")
+    else:
+        print("Invalid employee id number...")
 
 while True:
     dashboard()
@@ -56,10 +74,16 @@ while True:
             read_emp()
 
     if chioce == 3:
-        pass
+        if len(db) == 0:
+            print("Database is empty...")
+        else:
+            update_emp()
 
     if chioce == 4:
-        pass
+        if len(db) == 0:
+            print("Database is empty...")
+        else:
+            delete_emp()
 
 
     ch = input("Do you want continue [y/n] : ")
