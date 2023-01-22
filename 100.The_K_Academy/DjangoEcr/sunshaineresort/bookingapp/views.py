c79a9c1a2eaad51ac8f0c0f915ec3111986569ae
Tejas Falke <tejasdf.77@gmail.com>
2023-01-22 21:05:14 +0530

    add some extra info

--- 100.The_K_Academy/DjangoEcr/sunshaineresort/bookingapp/views.py
+++ 100.The_K_Academy/DjangoEcr/sunshaineresort/bookingapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
 
@@ -7,5 +8,20 @@ def index_view(request):
 
 def booking_view(request):
     print(request.method)
+    if request.method == "POST":
+        print(request.POST)
+        uname = request.POST.get('name')
+        email = request.POST.get('email')
+        mobile = request.POST.get('mobile')
+        adhar = request.POST.get('adhar')
+        date = request.POST.get('date')
+        days = request.POST.get('days')
+        status = request.POST.get('status')
+        person = request.POST.get('persons')
+        rtype = request.POST.get('rtype')
+
+        booking = Bookings(Name=uname, email_id=email, mobile=mobile, days=days, adhar_no=adhar, status = status, no_of_persons = person, booking_date=date, room_type=rtype)
+        booking.save()
+        return redirect('/display/')
 
     return render(request, 'bookingapp/bookingpage.html')
