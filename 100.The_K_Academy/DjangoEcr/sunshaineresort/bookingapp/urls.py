0528b98964336bc74f7b43bd0a2f260f521701ae
Tejas Falke <tejasdf.77@gmail.com>
2023-01-24 08:09:50 +0530

    main boot

--- 100.The_K_Academy/DjangoEcr/sunshaineresort/bookingapp/urls.py
+++ 100.The_K_Academy/DjangoEcr/sunshaineresort/bookingapp/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('index/', index_view, name ='index'),
-    path('booking/', booking_view, name='booking'),
+    path('index/', index_view),
+    path('booking/', booking_view),
+    path('display/', display_view),
+    path('update/<int:id>/', update_view),
+    path('delete/<int:id>/', delete_view),
 ]
--- 100.The_K_Academy/DjangoEcr/sunshaineresort/bookingapp/views.py
+++ 100.The_K_Academy/DjangoEcr/sunshaineresort/bookingapp/views.py
@@ -22,6 +22,47 @@ def booking_view(request):
 
         booking = Bookings(Name=uname, email_id=email, mobile=mobile, days=days, adhar_no=adhar, status = status, no_of_persons = person, booking_date=date, room_type=rtype)
         booking.save()
-        return redirect('/display/')
+        return redirect('/bookingapp/display/')
 
     return render(request, 'bookingapp/bookingpage.html')
+
+def display_view(request):
+    data = Bookings.objects.all()
+    context = {'data': data}
+    return render(request, 'bookingapp/display.html', context)
+
+def update_view(request, id):
+    data = Bookings.objects.get(pk = id)
+    context ={'data' : data}
+
+    if request.method == 'POST':
+        uname = request.POST.get('name')
+        email = request.POST.get('email')
+        mobile = request.POST.get('mobile')
+        adhar = request.POST.get('adhar')
+        date = request.POST.get('date')
+        days = request.POST.get('days')
+        status = request.POST.get('status')
+        person = request.POST.get('persons')
+        rtype = request.POST.get('rtype')
+
+        data.Name = uname
+        data.email_id = email
+        data.mobile = mobile
+        data.adhar_no = adhar
+        data.booking_date = date
+        data.status = status
+        data.days = days
+        data.no_of_persons = person
+        data.room_type = rtype
+        data.save()
+
+        return redirect('/bookingapp/display')
+
+
+    return render(request, 'bookingapp/update.html', context)
+
+def delete_view(request, id):
+    data = Bookings.objects.get(pk = id)
+    data.delete()
+    return redirect('/bookingapp/display/')
