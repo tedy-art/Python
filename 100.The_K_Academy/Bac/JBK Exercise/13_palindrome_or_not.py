914b1bec0db04a530f0322128d270c44f1795d23
Tejas Falke <tejasdf.77@gmail.com>
2023-02-21 08:20:54 +0530

    JBK Practice examples

--- 100.The_K_Academy/Bac/JBK Exercise/13_palindrome_or_not.py
+++ 100.The_K_Academy/Bac/JBK Exercise/13_palindrome_or_not.py
@@ -7,7 +7,26 @@ while temp > 0:
     rem = temp%10
     reverse = (reverse*10)+rem
     temp = temp//10
+
+print()
+print("Using simple Iteration method")
+
 if num == reverse:
-    print("Palindrome")
+    print(f"{num} is Palindrome")
 else:
-    print("Not palindrome")
+    print(f"{num} is Not palindrome")
+
+"""
+    Using string slicing
+"""
+
+num = 1234
+reverse = int(str(num)[::-1])
+
+print()
+print("Using simple string slicing method")
+
+if num == reverse:
+    print(f"{num} is Palindrome")
+else:
+    print(f"{num} is Not Palindrome")
