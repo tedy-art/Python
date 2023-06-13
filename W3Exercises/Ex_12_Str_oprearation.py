c13dc1d7cdcc58facb8f57729d03d8ead185eaac
Tejas Falke <tejasdf.77@gmail.com>
2023-06-13 18:44:21 +0530

    some basic problem solving

---
+++ W3Exercises/Ex_12_Str_oprearation.py
@@ -0,0 +1,13 @@
+"""
+Write a Python program to get a newly-generated string from a given string where "Is"
+has been added to the front. Return the string unchanged if the given string already
+begins with "Is"
+"""
+def new_str(text):
+    if len(text) >= 2 and text[:2] == "Is":
+        return text
+    return "Is" + text
+
+print(new_str("Array"))
+print(new_str("IsEmpty"))
+
